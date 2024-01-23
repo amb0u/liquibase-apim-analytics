2b19e50a9af6ea0604f2a0e26912e7aac2a94325
amb00u <ambroise.tine@gmail.com>
2024-01-23 16:41:41 +0100

    initial commit

---
+++ src/main/resources/db.changelog/migrations/init-schemas.sql
@@ -0,0 +1,6 @@
+CREATE SCHEMA IF NOT EXISTS "stats-db";
+/*None of the following databases need DB scripts. The tables will be automatically created.*/
+CREATE SCHEMA IF NOT EXISTS "wso2_dashboard_db";
+CREATE SCHEMA IF NOT EXISTS "business_rules_db";
+CREATE SCHEMA IF NOT EXISTS "wso2_permissions_db";
+CREATE SCHEMA IF NOT EXISTS "geo_locaion_data";
